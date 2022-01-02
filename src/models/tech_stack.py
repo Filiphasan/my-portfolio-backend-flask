1dbc162c49348e0c519d0da643c5790a86b278a6
Hasan Erdal <hasaerda@hotmail.com>
2022-01-02 18:23:46 +0300

    Edit tech stack

--- src/models/tech_stack.py
+++ src/models/tech_stack.py
@@ -8,11 +8,13 @@ class TechStackModel(db.Model, BaseModel):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(60), nullable=False)
-    icon = db.Column(db.String(150), nullable=False)
+    icon = db.Column(db.String(1000), nullable=False)
+    is_icon_devicon = db.Column(db.Boolean, default=True ,nullable=False)
 
-    def __init__(self, name: str, icon: str):
+    def __init__(self, name: str, icon: str, is_icon_devicon:bool):
         self.name = name
         self.icon = icon
+        self.is_icon_devicon = is_icon_devicon
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         self.is_deleted = False
