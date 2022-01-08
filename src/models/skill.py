883063787bfe6232dcdbe2053f7686274a9be47a
Hasan Erdal <hasaerda@hotmail.com>
2022-01-08 15:00:23 +0300

    Edit skill model

--- src/models/skill.py
+++ src/models/skill.py
@@ -9,14 +9,15 @@ class SkillModel(db.Model, BaseModel):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     skill_level = db.Column(db.Integer, nullable=True)
-    image_path = db.Column(db.String(250), nullable=True)
+    icon = db.Column(db.String(10000), nullable=False)
+    is_icon_svg = db.Column(db.Boolean, default=False, nullable=False)
     bg_color_from = db.Column(db.String(20), nullable=True)
     bg_color_to = db.Column(db.String(20), nullable=True)
 
-    def __init__(self, name: str, skill_level: int, image_path: str, bg_color_from: str, bg_color_to: str):
+    def __init__(self, name: str, skill_level: int, icon: str, bg_color_from: str, bg_color_to: str):
         self.name = name
         self.skill_level = skill_level
-        self.image_path = image_path
+        self.icon = icon
         self.bg_color_from = bg_color_from
         self.bg_color_to = bg_color_to
         self.created_at = datetime.now()
