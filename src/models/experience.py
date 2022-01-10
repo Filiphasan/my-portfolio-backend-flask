ee06b50b795c4f38078ab7449b99932e757756c1
Hasan Erdal <hasaerda@hotmail.com>
2022-01-10 16:26:10 +0300

    Edit experience model constructor method

--- src/models/experience.py
+++ src/models/experience.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 from db import db
 
 from src.models.base import BaseModel
@@ -13,11 +13,11 @@ class ExperienceModel(db.Model, BaseModel):
     end_date = db.Column(db.Date, nullable=False)
     description = db.Column(db.String(300), nullable=False)
 
-    def __init__(self, title: str, company: str, start_date: str, end_date: str, description: str):
+    def __init__(self, title: str, company: str, start_date: date, end_date: date, description: str):
         self.title = title
         self.company = company
-        self.start_date = datetime.strptime(start_date, "%d.%m.%Y").date()
-        self.end_date = datetime.strptime(end_date, "%d.%m.%Y").date()
+        self.start_date = start_date
+        self.end_date = end_date
         self.description = description
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
