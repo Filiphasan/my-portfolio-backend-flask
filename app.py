7694fd23cd334ee2668f0682e74668af4429c75a
Hasan Erdal <hasaerda@hotmail.com>
2022-01-17 20:26:50 +0300

    .

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = os.environ.get("SECRET_KEY", 'application_secret_key') #Example: c094b11d-8eb1-450b-949a-f83d9564c923
 db.init_app(app)
 ma.init_app(app)
-migrate = Migrate(app, db, compare_type=True) #compare_type=True, if change column property(length) alembic not detect, s
+migrate = Migrate(app, db, compare_type=True) #if change column property(length) alembic not detect, use 'compare_type=True' and alembic detect every f*cking changes :D
 
 # Implement Namespace In Api right below
 api.add_namespace(user_ns)
@@ -122,7 +122,7 @@ def handler_global_error(error):
     code = 500
     if isinstance(error, HTTPException):
         code = error.code
-    # return jsonify({"status":"error","message":"Ooops! Something went wrong!"}), code #This is for production.
+    # return jsonify({"status":"error", "status-code":code,"message":"Ooops! Something went wrong!"}), code #This is for production.
     return jsonify({"status":"error", "status-code":code,"message":str(error)}), code
 
 if __name__ == "__main__":
