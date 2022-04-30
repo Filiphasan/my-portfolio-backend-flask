f9a74606d88fbc2319b5cdd3acef58af85a84db3
Hasan Erdal <hasaerda@hotmail.com>
2022-04-30 23:01:57 +0300

    response düzenlemeleri

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,47 +1,47 @@
 def success_data_response(data, code: int):
     return {
         "status":"success",
-        "status-code":code,
+        "status_code":code,
         "data":data
     }, code
 
 def success_data_response_with_pagination(data, code: int, page: int, item_count: int, item_count_per_page: int, page_count: int):
     return {
         "status":"success",
-        "status-code": code,
+        "status_code": code,
         "data": data,
         "page_number": page,
         "count": item_count,
         "item_count_per_page": item_count_per_page,
         "page_count": page_count 
-    }
+    }, code
 
 def success_upload_response(file_path: str, server_url: str, message: str, code: int):
     return {
         "status":"success",
-        "status-code":code,
+        "status_code":code,
         "file": file_path,
-        "server-url": server_url,
+        "server_url": server_url,
         "message": message
-    }
+    }, code
 
 def success_token_response(token: str, code: int):
     return {
         "status":"success",
-        "status-code": code,
-        "access-token": token
+        "status_code": code,
+        "access_token": token
     }, code
 
 def success_response(message: str, code: int):
     return {
         "status":"success",
-        "status-code": code,
+        "status_code": code,
         "message": message
     }, code
 
 def error_response(message: str, code: int):
     return {
         "status":"error",
-        "status-code":code,
+        "status_code":code,
         "message":message
     }, code
